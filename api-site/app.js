540ea7c8baf29667b3045aa915168ad4f79dc84e
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2022-05-29 11:43:18 -0300

    mudando porta

--- api-site/app.js
+++ api-site/app.js
@@ -4,8 +4,7 @@ process.env.AMBIENTE_PROCESSO = "producao";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
-
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
